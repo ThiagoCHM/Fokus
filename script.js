8e58f7f15a695a79765584f7243011110a7772d7
Thiago C H Moreira <thiagochm@gmail.com>
2025-02-04 22:21:12 -0300

    feat: alterarContexto

---
+++ script.js
@@ -0,0 +1,47 @@
+const html = document.querySelector('html');
+const displayTempo = document.querySelector('#timer');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title')
+
+const focoBt = document.querySelector('.app__card-button--foco');
+const curtoBt = document.querySelector('.app__card-button--curto');
+const longoBt = document.querySelector('.app__card-button--longo');
+
+const duracaoFoco = 1500;
+const duracaoDescansoCurto = 300;
+const duracaoDescansoLongo = 900;
+
+focoBt.addEventListener('click', () => {
+    alterarContexto('foco');
+});
+
+curtoBt.addEventListener('click', () => {
+    alterarContexto('descanso-curto');
+});
+longoBt.addEventListener('click', () => {
+    alterarContexto('descanso-longo');
+});
+
+function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto);
+    banner.src = `/imagens/${contexto}.png`;
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `
+            Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            `
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `
+            Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>
+            `
+        default:
+            break;
+    }
+}
